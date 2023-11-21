bbf64cb5d7e0ab0a2482adf0744df9f6c414c2d6
dmitryzavyalov <zavyalovdmitry@mail.ru>
2023-11-21 19:51:28 -0300

    fix: new api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://j2mwfpi552.execute-api.us-east-2.amazonaws.com/dev",
-  order: "https://j2mwfpi552.execute-api.us-east-2.amazonaws.com/dev",
-  import: "https://j2mwfpi552.execute-api.us-east-2.amazonaws.com/dev",
-  bff: "https://j2mwfpi552.execute-api.us-east-2.amazonaws.com/dev",
-  cart: "https://j2mwfpi552.execute-api.us-east-2.amazonaws.com/dev",
+  product: "https://72em9ktbg9.execute-api.us-east-2.amazonaws.com/dev",
+  order: "https://72em9ktbg9.execute-api.us-east-2.amazonaws.com/dev",
+  import: "https://72em9ktbg9.execute-api.us-east-2.amazonaws.com/dev",
+  bff: "https://72em9ktbg9.execute-api.us-east-2.amazonaws.com/dev",
+  cart: "https://72em9ktbg9.execute-api.us-east-2.amazonaws.com/dev",
 };
 
 export default API_PATHS;
